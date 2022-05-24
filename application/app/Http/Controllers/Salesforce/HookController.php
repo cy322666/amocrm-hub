d5dd8a1e558e19c1f67b7bf4d208366e0018483d
cy322666 <cy322666@gmail.com>
2022-05-24 14:09:08 +0200

    local test 1

--- application/app/Http/Controllers/Salesforce/HookController.php
+++ application/app/Http/Controllers/Salesforce/HookController.php
@@ -5,10 +5,10 @@ namespace App\Http\Controllers\Salesforce;
 use App\Http\Controllers\Controller;
 use App\Models\Account;
 use App\Models\Salesforce\Hook;
-//use App\Services\amoCRM\Actions\AvitoHookSender;
 use App\Services\amoCRM\Actions\SalesforceHookSender;
 use App\Services\amoCRM\Client as amoApi;
-use App\Services\Salesforce\Client as Avito;
+use App\Services\Salesforce\Client;
+use App\Services\Salesforce\Client as Salesforce;
 use App\Services\Salesforce\Actions\amoCRMHookSender;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\Routing\ResponseFactory;
@@ -42,18 +42,33 @@ class HookController extends Controller
         ]);
     }
 
-    public function amocrm(Request $request, Avito $sfApi)
+    public function amocrm(Request $request)
     {
+        if ($request->toArray()['leads']['status'][0]['pipeline_id'] !== env('AMOCRM_PIPELINE_ID')) {
+
+            return;
+        }
+
         Log::info(__METHOD__, $request->toArray());
 
-        $leadId = $request->leadId;
+        $leadId   = $request->toArray()['leads']['status'][0]['lead_id'];
+        $statusId = $request->toArray()['leads']['status'][0]['status_id'];
 
         $hook = Hook::query()
             ->where('lead_id', $leadId)
             ->first();
 
         if ($hook) {
-            $result = (new amoCRMHookSender($hook, $sfApi))->send();
+            $hook->status_id = $statusId;
+            $hook->save();
+
+            $account = Account::query()
+                ->where('name', 'salesforce')
+                ->first();
+
+            $sfApi = new Client($account);
+
+            (new amoCRMHookSender($hook->refresh(), $sfApi))->send();
 
         } else
             Log::alert(__METHOD__.' : hook for lead_id not found '.$leadId);
--- application/app/Services/Salesforce/Actions/amoCRMHookSender.php
+++ application/app/Services/Salesforce/Actions/amoCRMHookSender.php
@@ -11,29 +11,30 @@ use Illuminate\Support\Facades\Log;
 class amoCRMHookSender
 {
     private Model $hook;
-    private Client $avito;//TODO ne avito!
+    private Client $salesforce;
 
-    public function __construct(Model $model, Client $avito)
+    public function __construct(Model $model, Client $salesforce)
     {
-        $this->hook  = $model;
-        $this->avito = $avito;
+        $this->hook = $model;
+        $this->salesforce = $salesforce;
     }
 
     public function send()
     {
-        $sfApi = (new Client(Account::query()
-            ->where('name', 'salesforce')
-            ->first())
-        );
+        try {
+            $response = $this->salesforce->services->update([
+                'id'     => $this->hook->salesforce_id,
+                'status' => Status::query()
+                    ->where('status_id', $this->hook->status_id)
+                    ->firstOrFail()
+                    ->status_name,
+            ]);
 
-        $response = $sfApi->services->update([
-            'id'     => $this->hook->salesforce_id,
-            'status' => Status::query()
-                ->where('status_id', $this->hook->status_id)
-                ->firstOrFail()
-                ->status_name,
-        ]);
+            Log::info(__METHOD__.' : '.$response);
 
-        Log::info(__METHOD__.' : '.$response);
+        } catch (\Throwable $exception) {
+
+            Log::error(__METHOD__. ' : '. $exception->getMessage());
+        }
     }
 }
