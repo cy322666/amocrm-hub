fb06a1cb3c3b9ac2920fb0463ff6cff50402cae4
cy322666 <cy322666@gmail.com>
2022-06-01 11:02:25 +0200

    hotfix url login

--- application/app/Services/Salesforce/Models/Base.php
+++ application/app/Services/Salesforce/Models/Base.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Log;
 
 class Base
 {
-    protected static string $baseUrl = 'https://avito.lightning.force.com/';
+    protected static string $baseUrl = 'https://avito.my.salesforce.com/';
 
     public Client $http;
     public $account;
